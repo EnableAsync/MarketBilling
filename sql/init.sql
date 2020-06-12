d9a83e50b2c5075491f78f025cf211f0de8e691e
SJJ <sunjiajunboy@gmail.com>
2020-06-12 18:12:15 +0800

    update sql file

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `market_billing` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `market_billing`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: market_billing
@@ -35,7 +33,7 @@ CREATE TABLE `bills` (
   `creator` varchar(50) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +42,7 @@ CREATE TABLE `bills` (
 
 LOCK TABLES `bills` WRITE;
 /*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES (1,'牙膏','黑人牙膏',1,20,1,1,'Admin','2020-06-05 17:00:22'),(2,'香皂','舒肤佳',1,5,1,2,'Admin','2020-06-06 11:04:07'),(3,'面包','桃李面包',1,4,1,3,'Admin','2020-06-06 11:04:50');
+INSERT INTO `bills` VALUES (1,'牙膏','黑人牙膏',1,20,1,1,'Admin','2020-06-05 17:00:22'),(2,'香皂','舒肤佳',1,5,1,1,'admin','2020-06-10 00:00:00'),(3,'面包','桃李面包',1,4,1,5,'admin','2020-06-10 00:00:00'),(4,'三明治','好吃的三明治',1,5,1,8,'admin','2020-06-10 00:00:00');
 /*!40000 ALTER TABLE `bills` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +92,7 @@ CREATE TABLE `users` (
   `role` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_username_uindex` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +101,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test','test','12345667899','测试地址','2020-06-05',0),(2,'admin','admin','12388888888','测试地址','2000-06-01',0),(3,'neu','neu','13888888888','辽宁','1990-06-01',0);
+INSERT INTO `users` VALUES (1,'test','test','12345667899','测试地址','2020-06-05',0),(2,'admin','admin','12388888888','测试地址','2000-06-01',0),(3,'neu','neu','13888888888','辽宁','1990-06-01',0),(4,'emm','456','123456789','辽宁省沈阳市','2000-01-07',1),(5,'测试用户','1','测试电话','测试地址','2020-06-09',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +114,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-06 11:11:51
+-- Dump completed on 2020-06-12 18:11:54
